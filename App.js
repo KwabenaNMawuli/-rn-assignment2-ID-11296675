ba56b3b8ac32509846f9548becc05a7234e41d7d
KwabenaNMawuli <150973838+KwabenaNMawuli@users.noreply.github.com>
2024-05-27 09:42:47 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 24 }}>
+        My name is <Text style={{ fontWeight: "bold" }}>Kwabena</Text>{" "}
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
